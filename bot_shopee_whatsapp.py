979345d1b46acefa436d87e7018f44b40296d80b
ofertassecretas <deco.coelho1981@gmail.com>
2026-05-19 14:04:44 -0300

    Refactor bot_shopee_whatsapp.py for stability

--- bot_shopee_whatsapp.py
+++ bot_shopee_whatsapp.py
@@ -201,11 +201,16 @@ def get_magalu_offers():
 
     logging.info("Buscando ofertas Magalu")
 
-    headers = {
-        "User-Agent":"Mozilla/5.0",
-        "Accept":"text/html,application/xhtml+xml",
-        "Accept-Language":"pt-BR,pt;q=0.9"
-    }
+   headers = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
+    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language":"pt-BR,pt;q=0.9,en-US;q=0.8",
+    "Accept-Encoding":"gzip, deflate, br",
+    "Connection":"keep-alive",
+    "Upgrade-Insecure-Requests":"1",
+    "Cache-Control":"max-age=0",
+    "Referer":"https://www.google.com/"
+}
 
     produtos=[]
 
@@ -213,11 +218,13 @@ def get_magalu_offers():
 
         url=random.choice(MAGALU_URLS)
 
-        r=requests.get(
-            url,
-            headers=headers,
-            timeout=20
-        )
+       session = requests.Session()
+
+r=session.get(
+    url,
+    headers=headers,
+    timeout=20
+)
 
         logging.info(
             f"Status Magalu: {r.status_code}"
